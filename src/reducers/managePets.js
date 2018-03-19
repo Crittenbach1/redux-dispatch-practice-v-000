95e84f8177f42da86a7301db9e3883c8982e1415
Cynthia Rittenbach <crittenbach@gmail.com>
2018-03-19 06:29:11 +0000

    automatically backed up by learn

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,8 +1,27 @@
 export let state;
 
 
-export function managePets(){
-}
+export function managePets(state = pets: [], action){
+  switch (action.type) {
+
+    case 'ADD_PET':
+      return Object.assign({}, state, {
+        pets: [
+          ...state.pets, action.pet
+        ]
+      })
+
+      case 'REMOVE_PET':
+        const removePet = state.pets.findIndex(pet => pet.id === action.id)
+        return Object.assign({}, state, {
+          pets: [...state.pets.slice(0, removePet), ...state.pets.slice(removePet + 1)
+          ]
+        })
+
+      default: 
+        return state;
+   }
+ }
 
 export function dispatch(){
 }
